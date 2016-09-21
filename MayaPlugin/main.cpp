271f94f8cbd9a224a3f0e032411ab69dafac29aa
David Wigelius <dawi14@student.bth.se>
2016-09-21 16:03:28 +0200

    mayaplugzz

---
+++ MayaPlugin/main.cpp
@@ -0,0 +1,103 @@
+#include "maya_includes.h"
+#include <iostream>
+
+using namespace std;
+
+MCallbackIdArray myCallbackArray;
+
+void WorldMatrixModified(MObject &transformNode, MDagMessage::MatrixModifiedFlags &modified, void *clientData)
+{
+	MGlobal::displayInfo(MFnTransform(transformNode).name() + " worldmatrix changed");
+}
+
+void elapsedTimeFunction(float elapsedTime, float lastTime, void*clientData)
+{
+	MGlobal::displayInfo("Current time: ");
+}
+
+EXPORT MStatus initializePlugin(MObject obj)
+{
+	MStatus res = MS::kSuccess;
+
+	MFnPlugin myPlugin(obj, "maya plinxxx", "1.0", "any", &res);
+	if (MFAIL(res))
+		CHECK_MSTATUS(res);
+
+	MGlobal::displayInfo("Maya plugin loaded!");
+
+	MStatus loopResults = MS::kSuccess;
+
+	MItDag meshIt(MItDag::kBreadthFirst, MFn::kTransform, &res);
+	for (; !meshIt.isDone(); meshIt.next())
+	{
+		MFnTransform trans = meshIt.currentItem();
+		if (trans.child(0).hasFn(MFn::kCamera) || trans.child(0).hasFn(MFn::kMesh))
+		{
+			MDagPath meshDag = MDagPath::getAPathTo(trans.child(0));
+			MCallbackId newId = MDagMessage::addWorldMatrixModifiedCallback(meshDag, WorldMatrixModified, NULL, &loopResults);
+			if (loopResults == MS::kSuccess)
+			{
+				if (myCallbackArray.append(newId) == MS::kSuccess)
+					MGlobal::displayInfo("added worldmatrix for " + trans.name());
+				else
+					MGlobal::displayInfo("failed to add worldmatrix callback");
+			}
+			else
+			{
+				MGlobal::displayInfo("failed to connect");
+			}
+		}
+
+
+		/*MDagPath meshDag = MDagPath::getAPathTo(meshIt.currentItem());
+		MCallbackId newId = MDagMessage::addWorldMatrixModifiedCallback(meshDag, WorldMatrixModified, NULL, &loopResults);
+		if (loopResults == MS::kSuccess)
+		{
+			if (myCallbackArray.append(newId) == MS::kSuccess)
+				MGlobal::displayInfo("added worldmatrix");
+			else
+				MGlobal::displayInfo("failed to add worldmatrix callback");
+		}
+		else
+		{
+			MGlobal::displayInfo("failed to connect");
+		}*/
+
+
+			//bajs = myCallbackArray.append(MDagMessage::addWorldMatrixModifiedCallback(meshDag, WorldMatrixModified, NULL, NULL));
+		//MFnTransform myMeshFn(meshIt.currentItem());
+		//MGlobal::displayInfo(myMeshFn.name());
+		/*if (meshIt.currentItem().hasFn(MFn::kMesh))
+		{
+			//MGlobal::displayInfo("mesh found!");
+			//MFnMesh kuk(meshIt.currentItem());
+			//MFn::kWorld
+			if (MFAIL(bajs))
+				MGlobal::displayInfo("failed to connest");
+		} */
+	}
+	MCallbackId newId = MTimerMessage::addTimerCallback(5, elapsedTimeFunction, NULL, &loopResults);
+	if (loopResults == MS::kSuccess)
+	{
+		if (myCallbackArray.append(newId) == MS::kSuccess);
+		{
+			MGlobal::displayInfo("timer made");
+		}
+	}
+	return res;
+}
+
+/*MStatus 	remove (unsigned int index)
+ 	Remove the array element at the given index. More...*/
+
+EXPORT MStatus uninitializePlugin(MObject obj)
+{
+	MFnPlugin plugin(obj);
+
+	//free resources here
+	MMessage::removeCallbacks(myCallbackArray);
+
+	MGlobal::displayInfo("Maya plugin unloaded!");
+
+	return MS::kSuccess;
+}
