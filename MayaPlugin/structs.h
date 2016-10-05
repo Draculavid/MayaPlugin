e85f4a48d2f81123494f6a956e229e92c6cc406e
David Wigelius <dawi14@student.bth.se>
2016-10-05 11:32:35 +0200

    added something

--- MayaPlugin/structs.h
+++ MayaPlugin/structs.h
@@ -25,3 +25,26 @@ struct Normals
 {
 	float x, y, z;
 };
+/*
+enum MsgType
+{
+CREATE,
+CHANGE,
+
+NUMBER_OF_TYPES
+};
+
+enum MsgContain
+{
+MATERIAL_NEW,
+TRANSFORM_NEW,
+CAMERA_NEW,
+MESH_NEW,
+TEXTURE_NEW,
+MATERAL_CHANGED,
+VERTEX_CHANGE,
+CAMERA_CHANGED,
+DELETED,
+
+NUMBER_OF_SETTINGS //if you wanna looop them all
+};*/
